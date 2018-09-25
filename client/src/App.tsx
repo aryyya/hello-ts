647914c43801177c95e2ca44c7ca45baead49468
Arya <arya@live.ca>
2018-09-24 18:48:34 -0700

    Add styles with styled components.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,14 @@
-import * as React from 'react';
+import * as React from 'react'
 
-import Greeter from './components/landing-page/greeter'
+import Greeter, { Gender } from './components/landing-page/greeter'
 
 class App extends React.Component {
 
   public render() {
     return (
       <div>
-        <Greeter name="John Smith" />
+        <Greeter name="Jill Smith" gender={Gender.FEMALE} />
+        <Greeter name="John Smith" gender={Gender.MALE} />
       </div>
     )
   }
--- client/src/components/landing-page/greeter.tsx
+++ client/src/components/landing-page/greeter.tsx
@@ -1,20 +1,49 @@
 import * as React from 'react'
+import styled from 'styled-components'
 
-interface IProps {
-  name: string
+export enum Gender {
+  MALE,
+  FEMALE
 }
 
-class Greeter extends React.Component<IProps> {
+interface IGreeterProps {
+  name: string,
+  gender: Gender
+}
+
+export default class Greeter extends React.Component<IGreeterProps> {
 
   public render () {
-    const { name } = this.props
+    const { name, gender } = this.props
 
     return (
-      <div>
-        Hello, {name}!
-      </div>
+      <GreeterStyled>
+        Hello, <Name gender={gender}>{name}</Name>!
+      </GreeterStyled>
     )
   }
 }
 
-export default Greeter
+interface IGreeterStyledProps {
+  textColor?: string,
+  textSize?: number
+}
+
+const GreeterStyled = styled<IGreeterStyledProps, 'div'>('div')`
+  font-family: sans-serif;
+  font-size: 1.25rem;
+  color: orange;
+  margin: 1rem;
+`
+
+interface INameProps {
+  gender: Gender
+}
+
+const Name = styled<INameProps, 'span'>('span')`
+  font-weight: bold;
+  text-decoration: underline;
+  color: ${({ gender }) =>
+    gender === Gender.FEMALE ? 'blue' : 'red'
+  };
+`
